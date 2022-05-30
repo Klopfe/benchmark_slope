6801502229fb157ebf53ad4882469bb4743d7558
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2022-05-30 16:11:19 +0200

    cluster_updates stated to true

--- solvers/hybrid.py
+++ solvers/hybrid.py
@@ -7,19 +7,19 @@ with safe_import_context() as import_ctx:
 
 class Solver(BaseSolver):
     name = 'hybrid'
-
     install_cmd = 'conda'
     requirements = ['slope']
+    parameters = {'cluster_updates': [True]}
     references = []
 
     def set_objective(self, X, y, alphas):
         self.X, self.y, self.alphas = X, y, alphas
-        self.run(1)
+        self.run(2)
 
     def run(self, n_iter):
         self.coef_, _, _, _ = hybrid_cd(
             self.X, self.y, self.alphas, max_epochs=n_iter, verbose=False,
-            tol=1e-12)
+            tol=1e-12, cluster_updates=self.cluster_updates)
 
     def get_result(self):
         return self.coef_
