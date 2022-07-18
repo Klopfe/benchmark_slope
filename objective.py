75b448b6f28faecac3df00f91546c78a63e3778f
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2022-07-18 16:12:44 +0200

    updated objective file

--- objective.py
+++ objective.py
@@ -8,15 +8,17 @@ from benchopt import BaseObjective
 
 class Objective(BaseObjective):
     name = "SLOPE"
-    parameters = {'reg': [0.02]}
+    parameters = {'reg': [0.1, 0.01, 0.001],
+                  'q': [0.2, 0.1, 0.05]}
 
-    def __init__(self, reg):
+    def __init__(self, reg, q):
+        self.q = q
         self.reg = reg
 
     def set_data(self, X, y):
         self.X, self.y = X, y
         self.n_samples, self.n_features = self.X.shape
-        self.alphas = self._get_lambda_seq(self.reg)
+        self.alphas = self._get_lambda_seq()
 
     def compute(self, beta):
         # compute residuals
@@ -41,11 +43,11 @@ class Objective(BaseObjective):
     def to_dict(self):
         return dict(X=self.X, y=self.y, alphas=self.alphas)
 
-    def _get_lambda_seq(self, reg):
+    def _get_lambda_seq(self):
         randnorm = stats.norm(loc=0, scale=1)
-        q = 0.5
+        q = self.q
         alphas_seq = randnorm.ppf(
             1 - np.arange(1, self.X.shape[1] + 1) * q / (2 * self.X.shape[1]))
         alpha_max = dual_norm_slope(self.X, self.y / len(self.y), alphas_seq)
 
-        return alpha_max * alphas_seq * reg
+        return alpha_max * alphas_seq * self.reg
