49b92d870b3f831f21ac7e9193af3f43f5f4d232
Johan Larsson <johanlarsson@outlook.com>
2022-08-09 17:01:41 +0200

    chore: fix flake8 errors

--- solvers/hybrid.py
+++ solvers/hybrid.py
@@ -19,7 +19,8 @@ class Solver(BaseSolver):
     def run(self, n_iter):
         self.coef_ = hybrid_cd(
             self.X, self.y, self.alphas, max_epochs=n_iter, verbose=False,
-            tol=1e-12, cluster_updates=self.cluster_updates, fit_intercept=False)[0]
+            tol=1e-12, cluster_updates=self.cluster_updates,
+            fit_intercept=False)[0]
 
     def get_result(self):
         return self.coef_
--- solvers/oracle.py
+++ solvers/oracle.py
@@ -15,14 +15,14 @@ class Solver(BaseSolver):
     def set_objective(self, X, y, alphas):
         self.X, self.y, self.alphas = X, y, alphas
         self.w_star = prox_grad(
-            X, y, alphas, max_epochs=10_000, tol=1e-14, fista=False, verbose=False,
-            fit_intercept=False)[0]
+            X, y, alphas, max_epochs=10_000, tol=1e-14, fista=False,
+            verbose=False, fit_intercept=False)[0]
         self.run(2)
 
     def run(self, n_iter):
         self.coef_ = oracle_cd(
-            self.X, self.y, self.alphas, max_epochs=n_iter, verbose=False, tol=1e-12,
-            w_star=self.w_star, fit_intercept=False)[0]
+            self.X, self.y, self.alphas, max_epochs=n_iter, verbose=False,
+            tol=1e-12, w_star=self.w_star, fit_intercept=False)[0]
 
     def get_result(self):
         return self.coef_
