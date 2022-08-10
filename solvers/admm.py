807475d08bb0ccb5c91ed43e2410eaf9e1ca21ea
Johan Larsson <13087841+jolars@users.noreply.github.com>
2022-08-10 13:37:13 +0200

    feat: add intercepts to the solvers and objective (#2)
    * feat: add intercepts to the solvers and objective
    
    * chore: fix lint errors

--- solvers/admm.py
+++ solvers/admm.py
@@ -1,7 +1,9 @@
-from benchopt import BaseSolver
-from benchopt import safe_import_context
+from benchopt import BaseSolver, safe_import_context
 from benchopt.stopping_criterion import SufficientDescentCriterion
+
 with safe_import_context() as import_ctx:
+    import numpy as np
+
     from slope.solvers import admm
 
 
@@ -13,13 +15,14 @@ class Solver(BaseSolver):
     requirements = ['slope']
     references = []
 
-    def set_objective(self, X, y, alphas):
+    def set_objective(self, X, y, alphas, fit_intercept):
         self.X, self.y, self.alphas = X, y, alphas
+        self.fit_intercept = fit_intercept
 
     def run(self, n_iter):
-        self.coef_ = admm(
-            self.X, self.y, self.alphas, fit_intercept=False, tol=1e-12,
-            max_epochs=n_iter)[0]
+        self.coef_, self.intercept_ = admm(
+            self.X, self.y, self.alphas, fit_intercept=self.fit_intercept,
+            tol=1e-12, max_epochs=n_iter)[:2]
 
     def get_result(self):
-        return self.coef_
+        return np.hstack((self.intercept_, self.coef_))
