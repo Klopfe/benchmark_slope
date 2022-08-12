40000e9529a8b90ea3d9c9923f3075ee59ee3073
Johan Larsson <johanlarsson@outlook.com>
2022-08-12 13:33:17 +0200

    chore: autoformat with black

--- solvers/pgd.py
+++ solvers/pgd.py
@@ -2,16 +2,15 @@ from benchopt import BaseSolver, safe_import_context
 
 with safe_import_context() as import_ctx:
     import numpy as np
-
     from slope.solvers import prox_grad
 
 
 class Solver(BaseSolver):
-    name = 'pgd'
-    parameters = {'fista': [False, True]}
+    name = "pgd"
+    parameters = {"fista": [False, True]}
 
-    install_cmd = 'conda'
-    requirements = ['slope']
+    install_cmd = "conda"
+    requirements = ["slope"]
     references = []
 
     def set_objective(self, X, y, alphas, fit_intercept, anderson=False):
@@ -21,9 +20,17 @@ class Solver(BaseSolver):
 
     def run(self, n_iter):
         self.coef_, self.intercept_ = prox_grad(
-            self.X, self.y, self.alphas, fista=self.fista, max_epochs=n_iter,
-            tol=1e-12, fit_intercept=self.fit_intercept,
-            anderson=self.anderson, verbose=False, gap_freq=10)[:2]
+            self.X,
+            self.y,
+            self.alphas,
+            fista=self.fista,
+            max_epochs=n_iter,
+            tol=1e-12,
+            fit_intercept=self.fit_intercept,
+            anderson=self.anderson,
+            verbose=False,
+            gap_freq=10,
+        )[:2]
 
     def get_result(self):
         return np.hstack((self.intercept_, self.coef_))
