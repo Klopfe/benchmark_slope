75611e4fa03790d942bb578751941a2bb7127c87
Johan Larsson <johanlarsson@outlook.com>
2022-08-12 13:34:11 +0200

    chore: autoformat with black

--- solvers/anderson.py
+++ solvers/anderson.py
@@ -2,15 +2,14 @@ from benchopt import BaseSolver, safe_import_context
 
 with safe_import_context() as import_ctx:
     import numpy as np
-
     from slope.solvers import prox_grad
 
 
 class Solver(BaseSolver):
-    name = 'anderson'
+    name = "anderson"
 
-    install_cmd = 'conda'
-    requirements = ['slope']
+    install_cmd = "conda"
+    requirements = ["slope"]
     references = []
 
     def set_objective(self, X, y, alphas, fit_intercept):
@@ -19,9 +18,17 @@ class Solver(BaseSolver):
 
     def run(self, n_iter):
         self.coef_, self.intercept_ = prox_grad(
-            self.X, self.y, self.alphas, fista=False, max_epochs=n_iter,
-            tol=1e-12, fit_intercept=self.fit_intercept, anderson=True,
-            verbose=False, gap_freq=10)[:2]
+            self.X,
+            self.y,
+            self.alphas,
+            fista=False,
+            max_epochs=n_iter,
+            tol=1e-12,
+            fit_intercept=self.fit_intercept,
+            anderson=True,
+            verbose=False,
+            gap_freq=10,
+        )[:2]
 
     def get_result(self):
         return np.hstack((self.intercept_, self.coef_))
