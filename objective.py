b7e6f7f9ed1a802c34f02867706604b0278b5a6a
Johan Larsson <johanlarsson@outlook.com>
2022-08-12 13:43:37 +0200

    chore: autoformat with black

--- objective.py
+++ objective.py
@@ -2,15 +2,16 @@ import numpy as np
 from benchopt import BaseObjective
 from numpy.linalg import norm
 from scipy import stats
-
 from slope.utils import dual_norm_slope
 
 
 class Objective(BaseObjective):
     name = "SLOPE"
-    parameters = {'reg': [0.1, 0.01, 0.001],
-                  'q': [0.2, 0.1, 0.05],
-                  'fit_intercept': [True, False]}
+    parameters = {
+        "reg": [0.1, 0.01, 0.001],
+        "q": [0.2, 0.1, 0.05],
+        "fit_intercept": [True, False],
+    }
 
     def __init__(self, reg, q, fit_intercept):
         self.q = q
@@ -32,24 +33,20 @@ class Objective(BaseObjective):
         diff = y - X @ beta - intercept
 
         # compute primal objective
-        p_obj = (1. / (2 * n_samples) * diff @ diff
-                 + np.sum(self.alphas * np.sort(np.abs(beta))[::-1]))
+        p_obj = 1.0 / (2 * n_samples) * diff @ diff + np.sum(
+            self.alphas * np.sort(np.abs(beta))[::-1]
+        )
 
         # Compute dual
         theta = diff
         theta /= max(1, self._dual_norm_slope(theta, self.alphas))
 
-        d_obj = (norm(y) ** 2 - norm(y - theta * n_samples) ** 2) / \
-                (2 * n_samples)
-        return dict(value=p_obj,
-                    duality_gap=p_obj - d_obj)
+        d_obj = (norm(y) ** 2 - norm(y - theta * n_samples) ** 2) / (2 * n_samples)
+        return dict(value=p_obj, duality_gap=p_obj - d_obj)
 
     def to_dict(self):
         return dict(
-            X=self.X,
-            y=self.y,
-            alphas=self.alphas,
-            fit_intercept=self.fit_intercept
+            X=self.X, y=self.y, alphas=self.alphas, fit_intercept=self.fit_intercept
         )
 
     def _dual_norm_slope(self, theta, alphas):
@@ -61,11 +58,11 @@ class Objective(BaseObjective):
         randnorm = stats.norm(loc=0, scale=1)
         q = self.q
         alphas_seq = randnorm.ppf(
-            1 - np.arange(1, self.X.shape[1] + 1) * q / (2 * self.X.shape[1]))
+            1 - np.arange(1, self.X.shape[1] + 1) * q / (2 * self.X.shape[1])
+        )
 
         alpha_max = self._dual_norm_slope(
-            (self.y - self.fit_intercept * np.mean(self.y)) / len(self.y),
-            alphas_seq
+            (self.y - self.fit_intercept * np.mean(self.y)) / len(self.y), alphas_seq
         )
 
         return alpha_max * alphas_seq * self.reg
