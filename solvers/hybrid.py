e6f0cfa88c5a639f806ae054d46140b0a0cdc9e3
Johan Larsson <johanlarsson@outlook.com>
2022-08-12 13:35:52 +0200

    chore: autoformat with black

--- solvers/hybrid.py
+++ solvers/hybrid.py
@@ -2,15 +2,14 @@ from benchopt import BaseSolver, safe_import_context
 
 with safe_import_context() as import_ctx:
     import numpy as np
-
     from slope.solvers.hybrid import hybrid_cd
 
 
 class Solver(BaseSolver):
-    name = 'hybrid'
-    install_cmd = 'conda'
-    requirements = ['slope']
-    parameters = {'cluster_updates': [True]}
+    name = "hybrid"
+    install_cmd = "conda"
+    requirements = ["slope"]
+    parameters = {"cluster_updates": [True]}
     references = []
 
     def set_objective(self, X, y, alphas, fit_intercept):
@@ -20,9 +19,15 @@ class Solver(BaseSolver):
 
     def run(self, n_iter):
         self.coef_, self.intercept_ = hybrid_cd(
-            self.X, self.y, self.alphas, max_epochs=n_iter, verbose=False,
-            tol=1e-12, cluster_updates=self.cluster_updates,
-            fit_intercept=self.fit_intercept)[:2]
+            self.X,
+            self.y,
+            self.alphas,
+            max_epochs=n_iter,
+            verbose=False,
+            tol=1e-12,
+            cluster_updates=self.cluster_updates,
+            fit_intercept=self.fit_intercept,
+        )[:2]
 
     def get_result(self):
         return np.hstack((self.intercept_, self.coef_))
