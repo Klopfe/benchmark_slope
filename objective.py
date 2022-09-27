a45d7d3929eff716979f99372c715144bdd2972d
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2022-09-27 12:39:12 +0200

    preprocess

--- objective.py
+++ objective.py
@@ -1,11 +1,10 @@
-from benchopt import BaseObjective, safe_import_context
-
-with safe_import_context() as import_ctx:
-    import numpy as np
-    from numpy.linalg import norm
-    from scipy import sparse, stats
-    from sklearn.feature_selection import VarianceThreshold
-    from sklearn.preprocessing import MaxAbsScaler, StandardScaler
+import numpy as np
+from benchopt import BaseObjective
+from numpy.linalg import norm
+from scipy import stats
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.preprocessing import MaxAbsScaler, StandardScaler
+from scipy import sparse
 
 
 class Objective(BaseObjective):
@@ -22,15 +21,11 @@ class Objective(BaseObjective):
         self.fit_intercept = fit_intercept
 
     def set_data(self, X, y):
-        # remove zero variance predictors
         X = VarianceThreshold().fit_transform(X)
-
-        # standardize
         if sparse.issparse(X):
             X = MaxAbsScaler().fit_transform(X).tocsc()
         else:
             X = StandardScaler().fit_transform(X)
-
         self.X, self.y = X, y
 
         self.n_samples, self.n_features = self.X.shape
